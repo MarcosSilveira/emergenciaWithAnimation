328711251de4d47ae04a3c21dd31617ea431fd3d
Henrique Manfroi da Silveira <henriquemanfroi@MacBook-Pro-de-Henrique.local>
2014-01-29 14:44:07 -0200

    Posto

---
+++ Emergência/Emergencia/DCPosto.m
@@ -0,0 +1,15 @@
+//
+//  DCPosto.m
+//  Emergencia
+//
+//  Created by Henrique Manfroi da Silveira on 29/01/14.
+//  Copyright (c) 2014 Acácio Veit Schneider. All rights reserved.
+//
+
+#import "DCPosto.h"
+
+@implementation DCPosto
+
+
+
+@end
