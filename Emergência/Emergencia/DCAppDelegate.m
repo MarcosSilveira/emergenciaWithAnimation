bf7de33497c0e2f9dd84339836a9edfa621438df
Marcos Sokolowski <Marcos@MacBook-Pro-de-Marcos.local>
2014-02-11 15:43:51 -0200

    -

--- Emergência/Emergencia/DCAppDelegate.m
+++ Emergência/Emergencia/DCAppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "DCAppDelegate.h"
 #import "DCLoginViewController.h"
+#import "DCMapasViewController.h"
 
 @implementation DCAppDelegate
 
@@ -22,21 +23,38 @@
     
     return YES;
 }
-							
+-(void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo{
+    
+    float latitude = [[userInfo objectForKey:@"lat"] floatValue];
+    float longitude =[[userInfo objectForKey:@"log"]floatValue];
+    MKPointAnnotation *amigo;
+    CLLocationCoordinate2D coordenada = CLLocationCoordinate2DMake(latitude, longitude);
+    amigo.coordinate = coordenada;
+    
+    
+    UINavigationController *navController = (UINavigationController *)self.window.rootViewController;
+    [navController popToRootViewControllerAnimated:NO];
+    
+    
+    DCLoginViewController *dcvc = (DCLoginViewController *)navController.viewControllers[0];
+    dcvc.coordenanda = coordenada;
+    
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-  // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-  // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-  // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)newDeviceToken {
-  
+    
     NSString *pushId = [[[[newDeviceToken description]
                           stringByReplacingOccurrencesOfString:@"<" withString:@""]
                          stringByReplacingOccurrencesOfString:@">" withString:@""]
@@ -52,17 +70,17 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-  // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-  // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-  // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
